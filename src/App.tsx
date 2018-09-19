7d95a73ab56feb9e7201ee54cab7538167874fc8
Jarryd Crawford <jarryd@bulb.co.uk>
2018-09-19 23:22:36 +0100

    Minor dev tooling updates and start of landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,49 @@
 import * as React from 'react';
-import './App.css';
+import styled, { injectGlobal } from 'react-emotion';
 
-import logo from './logo.svg';
+injectGlobal`
+  *,
+  *::before,
+  *::after {
+    box-sizing: border-box;
+  }
 
-class App extends React.Component {
-  public render() {
+  html,
+  body {
+    margin: 0;
+    padding: 0;
+    font-size: 100%;
+    font-family: serif;
+  }
+`;
+
+const Main = styled('main')`
+  display: flex;
+  height: 100vh;
+  flex: 1 0 auto;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+`;
+
+const Section = styled('section')`
+  padding: 20px;
+`;
+
+class App extends React.Component<{}> {
+  public render(): JSX.Element {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </div>
+      <Main>
+        <header>{/* Hamburger */}</header>
+        <Section>
+          <h1>Dodapp</h1>
+          <p>
+            Dodapp makes it easier for you to collect DOD and MOM votes, without
+            losing any of the crudeness or mockery.
+          </p>
+          <button>How it works</button>
+        </Section>
+      </Main>
     );
   }
 }

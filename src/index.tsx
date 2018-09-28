a1d4500deebf4ba71f62e9d35d8dea75a97a03ed
Jarryd Crawford <jarryd@bulb.co.uk>
2018-09-28 13:48:10 +0100

    Add basic Apollo and GraphQL setup

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,24 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { ApolloProvider } from 'react-apollo';
+
 import App from './App';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root') as HTMLElement
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: new HttpLink({
+    uri: '',
+  }),
+});
+
+const ApolloApp = (Component: React.ComponentClass) => (
+  <ApolloProvider client={client}>
+    <Component />
+  </ApolloProvider>
 );
+
+ReactDOM.render(ApolloApp(App), document.getElementById('root') as HTMLElement);
